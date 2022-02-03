29b735ac8c2a34c0e26a92726c9830921aad3ebf
AsilenA <3095016460@qq.com>
2022-02-04 02:07:30 +0800

    beautiful one

--- C programing results/4.test/test10.c
+++ C programing results/4.test/test10.c
@@ -2,7 +2,9 @@
 int main()
 {
     char ch;
-    scanf(" %c", &ch);
-    printf("astring%cAAA", ch);
+    scanf(" %c", &ch);                                           //测试在%号前加空格于不加的效果：不加空格则%c可以输入空格，而像现在这样就不能加空格
+    printf("astring%cAAA", ch);                                  //这是%c的输出效果
+    printf("changable number:%-*.*f", 12, 2, 1234.5678);         //可变字段宽度的应用办法和效果（用*修饰符实现）
+    printf("Count Beppo ran %10.2f miles in 3 hours.\n", 10.22); //试验合适的字段宽度（10是不必要的，有多余的空格，不合适）
     return 0;
 }

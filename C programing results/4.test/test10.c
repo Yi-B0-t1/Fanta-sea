20fe6f4df09c1c7ef0d2040558a06f21350f0fc8
AsilenA <3095016460@qq.com>
2022-02-05 04:54:20 +0800

    today's work

--- C programing results/4.test/test10.c
+++ C programing results/4.test/test10.c
@@ -6,5 +6,6 @@ int main()
     printf("astring%cAAA", ch);                                  //这是%c的输出效果
     printf("changable number:%-*.*f", 12, 2, 1234.5678);         //可变字段宽度的应用办法和效果（用*修饰符实现）
     printf("Count Beppo ran %10.2f miles in 3 hours.\n", 10.22); //试验合适的字段宽度（10是不必要的，有多余的空格，不合适）
+    printf("%c%c%c\n", 'H', 105, '\41');
     return 0;
 }

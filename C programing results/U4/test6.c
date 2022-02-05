eccd19bdbdfc2d249a2a3f6fc234ad3a9c3766db
AsilenA <3095016460@qq.com>
2022-02-06 00:51:39 +0800

    reall start

--- C programing results/U4/test6.c
+++ C programing results/U4/test6.c
@@ -1,9 +1,14 @@
 /* pizza.c -- 在比萨饼程序中使用已定义的常量 */
-#include<stdio.h>
-int main(){
-    int x=-002;char charr='4';float flo=2.303;
-    printf("***int=%04.2d***,char=%0c,***float=%010f***\n",x,charr,flo);
-    char char1='9';char char2=6;
-    printf("char1=%c,char1hh=%hhc,char2=%c,char2=%hhc",char1,char1,char2,char2);
+#include <stdio.h>
+int main()
+{
+    int x = -002;
+    char charr = '4';
+    float flo = 2.303;
+    printf("***int=%04.2d***,char=%0c,***float=%010f***\n", x, charr, flo);
+    char char1 = '9';
+    char char2 = 6;
+    printf("char1=%c,char1hh=%hhc,char2=%c,char2=%hhc\n", char1, char1, char2, char2);
+    printf("你的狗屎有%d块", 1000);
     return 0;
 }

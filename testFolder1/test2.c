69462e78245c9d64fa3c2bebab4cfc0dffa5e710
AsilenA <3095016460@qq.com>
2022-02-10 03:05:12 +0800

    better

--- testFolder1/test2.c
+++
@@ -1,9 +0,0 @@
-/* pizza.c -- 在比萨饼程序中使用已定义的常量 */
-#include<stdio.h>
-int main(){
-    int x=-002;char charr='4';float flo=2.303;
-    printf("***int=%04.2d***,char=%0c,***float=%010f***\n",x,charr,flo);
-    char char1='9';char char2=6;
-    printf("char1=%c,char1hh=%hhc,char2=%c,char2=%hhc",char1,char1,char2,char2);
-    return 0;
-}

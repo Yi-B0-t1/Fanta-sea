4516a2bb91be96ec5f72768f321e87c54bb6d243
AsilenA <3095016460@qq.com>
2022-03-03 11:31:52 +0800

    study

--- C language/U5/whichisthe1st.c
+++ C language/U5/whichisthe1st.c
@@ -1,11 +1,25 @@
+/*#include <stdio.h>
+int main()
+{
+    int a = 1;
+    int b = (3 > 5);
+    int x = 1, y = 2;
+    double z = -.3;
+    int c = 7, d = -2;
+    char ch = '%';
+    printf("result_____%d_____\n", a + 'a');
+    printf("result_____%d_____\n", sizeof(int));
+    printf("%c and %%\n", ch);
+    printf("test ''a number:___%d__%d__", a, 5 % -2);
+    return 0;
+}*/
 #include <stdio.h>
 int main()
 {
-    int a = 4 % 3 * 9;
-    int b = (3 > 5);
-    char ch;
-    ch = '%';
-    printf("%c and %%\n", ch);
-    printf("test ''a number:___%d__%d__", a, b);
+    int i, j, k;
+    i = 3;
+    j = 4;
+    k = 5;
+    printf("%d", !(i + j) + k - 1 && j + k / 2);
     return 0;
 }

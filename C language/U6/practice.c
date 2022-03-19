f44ee34ddf7be29fd497bbf85587a55a42529af9
AsilenA <3095016460@qq.com>
2022-03-19 19:30:26 +0800

    u7

--- C language/U6/practice.c
+++ C language/U6/practice.c
@@ -7,10 +7,10 @@ int main() /*
      for (i = 0; i < 26; i++, a++)
      {
          ABCarray[i] = a;
-         printf("%c ", ABCarray[i]);
+         printf("%c ", ABCarray[i]);//正序打印字母表
      }
      return 0;
- }*/
+ }
 {
     int i;
     double a, sum1 = 1.0, sum2 = 1.0;
@@ -18,11 +18,39 @@ int main() /*
     scanf("%d", &i);
     for (a = 2.0; (double)(i + 1) != a; a++)
     {
-        sum1 += (1.0 / a) * pow((-1), ((int)a % 2 + 1));
+        sum1 += (1.0 / a) * pow((-1), ((int)a % 2 + 1));//用-1的幂函数控制符号
         sum2 += (1.0 / a);
     }
     printf("\nsum is__%f__", sum1 + sum2);
-    printf("\nsum is__%f__", sum1);
-    printf("\nsum is__%f__", sum2);
+    printf("\nsum is__%f__", sum1);//收敛
+    printf("\nsum is__%f__", sum2);//发散
+    return 0;
+}*/
+{
+    double first[8], sum[8];
+    int i, s;
+
+    printf("Enter 8 numbers please:\n");
+    for (i = 0; i != 8; i++)
+    {
+        printf("No.%d number:", i + 1);
+        s = scanf("%lf", &first[i]);
+        if (1 != s)
+            break;
+        sum[i] = (0 == 1) ? first[0] : (sum[i - 1] + first[i]);
+    }
+
+    if (1 == s)
+    {
+        printf("first array:");
+        for (i = 0; i != 8; i++)
+            printf("%-6.3f ", first[i]);
+        printf("\nsum array  :");
+        for (i = 0; i != 8; i++)
+            printf("%-6.3f ", sum[i]);
+    }
+    else
+        printf("please check the form,you should enter the numbers!");
+
     return 0;
 }

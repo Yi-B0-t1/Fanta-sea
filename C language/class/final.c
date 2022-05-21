bfbd3f2dd8678418ff0794ffff06354476c556e6
AsilenA <3095016460@qq.com>
2022-05-22 05:16:52 +0800

    >_<

--- C language/class/final.c
+++ C language/class/final.c
@@ -7,26 +7,15 @@ int main(void)
         switch (menu_select()) //主菜单
         {
         case 1:
-            system("cls"); /* 清屏 */
-            // input stream loaded
-            puts("NOW you are in: Initialize from 3 files\n");
-            if (openfiles()) //注意，不能把函数原型直接拷贝过来用，把类型名字去掉
-            {                // create lists
-                init();
-                create_grade_list();
-                //一鼓作气吧这写完吧，抄也罢，省也罢，完成是第一目标
-                sort_slist();      //按照学号升序排序。
-                sort_clist();      //按照课程号升序排序。
-                sort_grade_list(); //按照平均成绩降序排序
-            }
-            printf("\n%f\n", grade_list[1].average_grade);
+            menu1();
             break;
         case 2:
             menu2();
             break;
         case 3:
-            /*键入与修改数据*/
+            menu3();
             break;
+            /*键入与修改数据*/
         case 4:
             /*根据输入删除对应数据*/
             break;
